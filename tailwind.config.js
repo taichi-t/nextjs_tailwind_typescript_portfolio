20b5b995d24722db40b705a14c18d176b50bf964
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-06-16 03:44:58 -0700

    bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge: ['./components/**/*.tsx', './pages/**/*.ts'],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        background: 'var(--color-background)',
+        primaryText: 'var(--color-primaryText)',
+        secondaryText: 'var(--color-secondaryText)',
+        disabled: 'var(--color-disabled)',
+        hint: 'var(--color-hint)',
+        divider: 'var(--color-divider)',
+        yellow: 'var(--color-yellow)',
+        blue: 'var(--color-blue)',
+        bioret: 'var(--color-bioret)',
+        red: 'var(--color-red)',
+        green: 'var(--color-green)',
+      },
     },
   },
   variants: {},
