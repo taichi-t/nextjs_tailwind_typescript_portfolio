87df91b886a6588c5fedff2e39879262370ef8e3
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-06-15 21:25:30 -0700

    fix:bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  purge: ['./components/**/*.js', './pages/**/*.js'],
+  purge: ['./components/**/*.tsx', './pages/**/*.ts'],
   theme: {
     extend: {},
   },
   variants: {},
   plugins: [],
-}
+};
