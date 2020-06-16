9812107770efc58ce497095bc41c4758bec39a16
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-06-16 04:00:54 -0700

    setup: tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
-import Nav from '../components/nav'
+import Nav from '../components/nav';
+import { useTheme } from '../utils/themeContext';
 
 export default function IndexPage() {
+  const { theme } = useTheme();
   return (
-    <div>
+    <div className={`${theme}`}>
       <Nav />
       <div className="hero">
-        <h1 className="title">Next.js + Tailwind CSS</h1>
+        <h1 className="title text-yellow">Next.js + Tailwind CSS</h1>
       </div>
     </div>
-  )
+  );
 }
