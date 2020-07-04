ff309e40e110fe10ecb55a1f2aeb974f5d5dd720
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-07-04 02:48:19 -0700

    refactoring:header,tags

--- components/About/ProfileImage/ProfileImage.tsx
+++ components/About/ProfileImage/ProfileImage.tsx
@@ -1,8 +1,24 @@
-export default function Aside() {
+import { useState, useRef, useEffect } from 'react';
+export default function ProfileImage() {
+  const [isImgLoaded, setIsImgLoaded] = useState(false);
+  const imgRef = useRef(null);
+  useEffect(() => {
+    if (imgRef.current.complete) setIsImgLoaded(true);
+  }, []);
+
+  const handleLoad = () => setIsImgLoaded(true);
   return (
     <div className="p-3 mobile:p-1">
-      <div>
-        <img src="/images/me.JPG" alt="me" className="circle" />
+      <div className={` ${!isImgLoaded && 'skeleton circle'}`}>
+        <img
+          src="/images/me.JPG"
+          alt="me"
+          className={`circle ${!isImgLoaded && 'invisible'}`}
+          width="100%"
+          height="100%"
+          ref={imgRef}
+          onLoad={handleLoad}
+        />
       </div>
     </div>
   );
