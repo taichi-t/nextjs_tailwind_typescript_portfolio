4f130f55ba0d2e6e81792382d613dc78029316dd
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-07-09 04:32:12 -0700

    add:favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,12 +7,36 @@ export default class Document extends NextDocument {
     return (
       <Html lang="en">
         <Head>
-          <link rel="manifest" href="/manifest.json" />
           <meta charSet="utf-8" />
           <meta
             name="viewport"
             content="initial-scale=1.0, width=device-width"
           />
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicons/favicon-16x16.png"
+          />
+          {/* <link rel="manifest" href="/site.webmanifest" /> */}
+          <link
+            rel="mask-icon"
+            href="/favicons/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <meta name="msapplication-TileColor" content="#2b5797" />
           <meta
             key="description"
             name="description"
