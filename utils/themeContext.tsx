aeffa805ec7f97e6b1e82de6c4087e6b6e60d0eb
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-10-28 00:58:49 -0700

    change: themeContext to typeScript

--- utils/themeContext.tsx
+++ utils/themeContext.tsx
@@ -1,25 +1,26 @@
-import { useState, useContext, createContext, useEffect } from 'react';
+import { useState, useContext, createContext, useEffect,ReactNode,FC } from 'react';
 import useFontFaceObserver from '@/hooks/useFontFaceObserver';
-const initialValue = { theme: 'theme-light', loaded: false };
 
-type ContextProps = {
+interface ContextProps {
   toggleTheme(): void;
-
   theme: string;
   loaded: boolean;
+  children:ReactNode
 };
+
+const initialValue = { theme: 'theme-light', loaded: false };
 
 export const ThemeContext = createContext<Partial<ContextProps>>(initialValue);
 
-export default function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState<string>('theme-dark');
-  const [loaded, setLoaded] = useState<boolean>(false);
+export default function ThemeProvider({ children }:ContextProps){
+  const [theme, setTheme] = useState('theme-dark');
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     useFontFaceObserver(setLoaded);
   }, []);
 
-  function toggleTheme() {
+  function toggleTheme(){
     setTheme(theme === 'theme-light' ? 'theme-dark' : 'theme-light');
   }
 
